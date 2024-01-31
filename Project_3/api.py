bfe197fdb3d4d6a3ae2ff431bb231f5b7ae212f4
Hanen Zeddini <hanen.zeddini@gmail.com>
2024-01-30 17:15:35 -0800

    adding leaflet time line slider

--- Project_3/api.py
+++ Project_3/api.py
@@ -2,18 +2,25 @@ from flask_cors import CORS
 import pandas as pd
 import os
 from flask import Flask, jsonify
+
 # Flask Setup: Initialize the Flask app.
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:5000"}})
 df = pd.read_csv('final.csv')
+
 #define destination api.
 @app.route("/api/v1.0/population/<year>")
 def population(year):
-    population_year = df[df['year'] == int(year)]
-    print(population_year)
+    population_year = df[df['year'] == int(year)] 
     data_dic = [{'year':x['year'], 'lat': x["lat"], 'lon': x['lon'], 'population': x['population'], 'zip_code': x['zip_code']} for _,x in population_year.iterrows()]
-
     return jsonify(data_dic)
+
+@app.route("/api/v1.0/years")
+def years():
+    years = df['year'].unique()  
+    data_dic = [{'years':int(x)} for x in years]
+    return jsonify(data_dic)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Project_3/front_end/static/js/leaflet-timeline-slider.js
+++ Project_3/front_end/static/js/leaflet-timeline-slider.js
@@ -1,7 +1,4 @@
-// This code is reference to this github repo :https://github.com/svitkin/leaflet-timeline-slider
-
-///////////////// Code ///////////////////
-
+// TODO: parameterize timeline colors, overall length, and length between points (css styles)
 L.Control.TimeLineSlider = L.Control.extend({
 
     options: {
@@ -316,78 +313,3 @@ L.Control.TimeLineSlider = L.Control.extend({
 L.control.timelineSlider = function(options) {
     return new L.Control.TimeLineSlider(options);
 }
-
-//////////////// End Code ///////////////////
-
-
-let baseUrl = "http://127.0.0.1:5000/api/v1.0/population/"
-let url = "http://127.0.0.1:5000/api/v1.0/population/2018"
-let years = "http://127.0.0.1:5000/api/v1.0/years"
-
-// Creating the map object
-let myMap = L.map("map", {
-    center: [63.5888, -154.4931],
-    zoom: 5
-  });
-  
-  // Adding the tile layer
- let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
-  
-// Create a chooseradius function:
-function markerSize(population) {
-    return Math.sqrt(population) * 300;
-}
-
-function getYearColor(year) {
-    const colors = ["#00A5E3", "#FFA23A", "#FF96C5", "#FF5768", "#FFBF65", "#6C88C4", "#FF3357", "#3357FF"];
-    
-    // Map the year to an index in the colors array
-    const index = (year - 2011) % colors.length;
-    
-    return colors[index];
-}
-
-
-getDataAddMarkers = function( {label, value, map} ) {
-    map.eachLayer(function (layer) {
-            if (layer instanceof L.CircleMarker) {
-                map.removeLayer(layer); 
-             
-            }
-               
-    });
-
-    markers = L.layerGroup();
-    markers.clearLayers()
-    var zipCodeMarker = []
-
-    d3.json(baseUrl+label).then(data => { 
-        for (let i= 0; i<data.length; i++) {
-            zipCodeMarker.push(
-                L.circle([data[i]['lat'], data[i]['lon']], {
-                    stroke: false,
-                    fillOpacity: 0.5,
-                    color: getYearColor(label),
-                    fillColor: getYearColor(label),
-                    radius: markerSize(data[i]['population'])
-                }).bindPopup(`<h2>ZIP Code: ${data[i]["zip_code"]}</h2> <hr> <h3>Population: ${data[i]['population'].toLocaleString()}</h3> <hr> <h3>Year: ${data[i]['year']}</h3>`)
-            ) 
-    
-        } 
-        let zipCodes = L.layerGroup(zipCodeMarker)
-        zipCodes.addTo(map);
-      
-    }) 
-    
-};
-
-d3.json(years).then(data => { 
-    years = data.map(x=>String(x.years))
-    L.control.timelineSlider({
-        timelineItems: years, 
-        changeMap: getDataAddMarkers
-     }).addTo(myMap); 
-
-})  
